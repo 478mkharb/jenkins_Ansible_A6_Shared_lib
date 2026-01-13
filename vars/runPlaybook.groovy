b99eabc5f85b7a4fb6e36d687fad95bb19d9907b
Mukesh Kharb <478mkharb@gmail.com>
2026-01-13 14:45:42 +0530

    Updated runPlaybook.groovy

--- vars/runPlaybook.groovy
+++ vars/runPlaybook.groovy
@@ -1,9 +1,16 @@
 def call(Map cfg) {
-    dir(cfg.CODE_BASE_PATH) {
-        sh """
-          ansible-playbook ${cfg.ANSIBLE.PLAYBOOK} \
-          -i ${cfg.ANSIBLE.INVENTORY} \
-          -f ${cfg.ANSIBLE.FORKS}
-        """
+
+    withCredentials([[
+        $class: 'AmazonWebServicesCredentialsBinding',
+        credentialsId: 'my-aws-cred'
+    ]]) {
+
+        dir(cfg.CODE_BASE_PATH) {
+            sh """
+              ansible-playbook ${cfg.ANSIBLE.PLAYBOOK} \
+              -i ${cfg.ANSIBLE.INVENTORY} \
+              -f ${cfg.ANSIBLE.FORKS}
+            """
+        }
     }
 }
